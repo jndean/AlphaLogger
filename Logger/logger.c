11ea871e7efbc657dc7287a84b170df6cb445f1d
jndean <email@josefdean.co.uk>
2020-07-18 16:51:10 +0100

    Unify memory in LoggerState into a single allocation, and refactor so the board is channels_last instead of channels_first (for later compatability with certain keras layers)

--- Logger/logger.c
+++ Logger/logger.c
@@ -4,59 +4,109 @@
 #include<stdio.h>
 #include<stddef.h>
 #include<stdlib.h>
+#include<stdint.h>
 
 #include<Python.h>
 #include <numpy/arrayobject.h>
 
-#define ALLOC_ERROR() do {printf("alloc error\n"); exit(1701);} while(0);
-
-
-
 // ---------------------------- LoggerState ---------------------------- //
+
+
 /*
-For P players, LoggerState contains 5 + 3P layers of 5x5 signed bytes.
-Positions are marked with 1, empty spaces with -1 layers passed to the NN
-The layers are:
-- Occupation (not passed to NN, 1 for occupied, 0 elsewhere)
-- Saplings
-- Young trees
-- Mature trees
-- Protesters
-- For each Player:
-  - Location
-  - Score
-  - Num protesters
+LoggerState just allocates one big block of memory of size
+and stores different stuff at different offsets such that the NN can just
+read portions of it as a single contiguous input tensor
+Unoccupied grid (5x5 bools) comes first, then the legal_moves tensor (5x5x10 bools)
+followed by the board grid (5x5x(5+3*num_players))
+The channels are:
+0: saplings               (1 / -1)
+1: young trees            (1 / -1)
+2: mature trees           (1 / -1)
+3: protesters             (1 / -1)
+4 onwards: 3 channels per player
+        0: position        (1 for player, -1 elsewhere)
+        1: score           (whole channel holds value)
+        2: num_protesters  (whole channel holds value)
 */
 
-typedef signed char Layer[25];
+#define SAPLINGS    0
+#define YOUNGTREES  1
+#define MATURETREES 2
+#define PROTESTERS  3
+#define PLAYERS     4
+
 
 typedef struct {
-    unsigned char num_players;
-    Layer* layers;
+	int8_t y;
+    int8_t x;
+} Position;
+
+
+typedef struct {
+	int8_t* unoccupied;
+	int8_t* legal_moves;
+	int8_t* board;
+	int stride;
+    Position player_positions[4];
+    uint8_t num_players;
+    uint8_t num_unprotested_trees;
+
 } LoggerState;
 
 
-
-int LoggerState_init(LoggerState* state, unsigned char num_players) {
+/* 
+  Initialise the memory for a LoggerState struct.
+  Doesn't set the memory to a valid game state.
+*/
+int LoggerState_init(LoggerState* state, uint8_t num_players) {
 
     state->num_players = num_players;
-    state->layers = malloc((5 + 3 * num_players) * sizeof(Layer));
-    if (state->layers == NULL)
+    state->stride = 4 + 3 * num_players;
+    state->unoccupied = malloc(25 * (1 + 10 + 4 + 3 * num_players));
+    if (state->unoccupied == NULL)
     	return 0;
+    state->legal_moves = state->unoccupied + 25;
+    state->board = state->unoccupied + 25 * 11;
 
     return 1;
 }
 
+
+/* 
+  Uninitialise the memory in a LoggerState struct. 
+  Doesn't free the LoggerState struct.
+*/
 void LoggerState_uninit(LoggerState* state) {
-	if (state->layers != NULL) free(state->layers);
+	if (state->unoccupied != NULL) 
+		free(state->unoccupied);
 }
 
+
+/* 
+  Set the game state to a new game with random player positions
+*/
 void LoggerState_reset(LoggerState* state) {
-	memset(state->layers[0], 0, sizeof(Layer));
-	memset(state->layers[1], -1, 4 * sizeof(Layer));
+
+	memset(state->unoccupied, 1, 25);
+	memset(state->board, -1, 25 * (4 + 3 * state->num_players));
+	for (int xy = 0; xy < 25; ++xy) {
+		int8_t *pos = state->board + state->stride * xy + PLAYERS + 1;
+		for (int p = 0; p < state->num_players; ++p, pos += 3) {
+			*pos = 0;
+			*(pos + 1) = 1;
+		}
+	}
+
+	int corner;
+	char corners[4] = {0, 0, 0, 0};
 	for (int p = 0; p < state->num_players; ++p) {
-		memset(state->layers[5 + 3 * p], 0, 2 * sizeof(Layer));
-		memset(state->layers[5 + 3 * p + 2], 1, sizeof(Layer));
+		do {corner = rand() & 3;} while(corners[corner]);
+		corners[corner] = 1;
+		int8_t x = (corner & 1) << 2;
+		int8_t y = (corner & 2) << 1;
+		state->player_positions[p].x = x;
+		state->player_positions[p].y = y;
+		state->board[(5 * y + x) * state->stride + PLAYERS + 3 * p] = 1;
 	}
 }
 
@@ -103,9 +153,9 @@ PyLoggerState_init(PyLoggerState *self, PyObject *args, PyObject *kwds)
 static PyObject*
 PyLoggerState_toarray(PyLoggerState *self, PyObject *Py_UNUSED(ignored)) 
 {
-	npy_intp dims[] = {4 + 3 * self->content.num_players, 5, 5};
+	npy_intp dims[] = {5, 5, self->content.stride};
 	PyObject *arr = PyArray_SimpleNewFromData(
-		3, dims, NPY_INT8, self->content.layers[1]
+		3, dims, NPY_INT8, self->content.board
 	);
 
 	return arr;
@@ -171,6 +221,8 @@ static struct PyModuleDef loggermodule = {
 
 PyMODINIT_FUNC PyInit_logger(void)
 {
+   	time_t t;
+  	srand((unsigned) time(&t));
 
 	import_array();
 
